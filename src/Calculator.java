47edbe0dfead32452a3891ca0a8323e7335ee422
DomSSD <valievrusik@mail.ru>
2022-01-05 16:33:15 +0300

    добавил строку для удаления пробелов в строке

--- src/Calculator.java
+++ src/Calculator.java
@@ -38,25 +38,36 @@ public class Calculator {
 // считали введенную строку м присволили ее значение expr
         Scanner scanner = new Scanner(System.in);
         String expr = scanner.nextLine();
+        expr = expr.replaceAll(" ", "");
+
 //        строку превратили в массив char-ов
         char[] arr = expr.toCharArray();
+
 //        узнали длину массива
         int lengthArr = arr.length;
+
 //        вывели в консоль сам массив
         System.out.println(arr);
+
 //        вывели в консоль длину массива
         System.out.println(lengthArr);
+
 //        создали массив на основе введеного выражения разделив его по знаку операции
         String[] s = expr.split("[\\+\\-\\*\\/]");
+
 //        вывеоли в консоль массив строк с нашими операндами
         System.out.println(Arrays.toString(s));
+
 //        узнали индекс в выражении знака операции
         int indexOfOperation = expr.indexOf("+");
+
 //        вывели в консоль номер или индекс знака операции
         System.out.println(indexOfOperation);
+
 //        присвоили и вывели в консоль знак операции
         operation = expr.charAt(indexOfOperation);
         System.out.println(operation);
+
 //       назначили и вывели в консоль операнды
         first = Integer.parseInt(s[0]);
         second = Integer.parseInt(s[1]);
